807e03d5841d8c3855ebb2b667a78491bc74c67b
bekatodua <beka.todua.1@btu.edu.ge>
2021-12-13 12:41:41 +0400

    updated post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $table = 'posts';
+
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
+    protected $fillable = [
+        'title',
+        'text',
+        'created_at',
+        'updated_at',
+    ];
 }
